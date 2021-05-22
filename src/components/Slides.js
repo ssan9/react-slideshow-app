4c5e2e5110d72623b0ba9e42bacc8423fbb15da0
ssan9 <perrsy@gmail.com>
2021-05-21 23:07:41 -0400

    added logic to make the slides functional

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,49 @@
 import React from 'react';
 
-function Slides({slides}) {
+class Slides extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+        this.restartButton = this.restartButton.bind(this);
+        this.prevButton = this.prevButton.bind(this);
+        this.nextButton = this.nextButton.bind(this);
+    }
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
+    restartButton() {
+        this.setState({
+            count: 0
+        });
+    }
 
+    prevButton() {
+        this.setState({
+            count: this.state.count - 1
+        });
+    }
+
+    nextButton() {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div id="navigation" className="text-center">
+                    <button data-testid="button-restart" className="small outlined" disabled={this.state.count === 0} onClick={this.restartButton}>Restart</button>
+                    <button data-testid="button-prev" className="small" disabled={this.state.count === 0} onClick={this.prevButton}>Prev</button>
+                    <button data-testid="button-next" className="small" disabled={this.state.count === this.props.slides.length - 1 } onClick={this.nextButton}>Next</button>
+                </div>
+                <div id="slide" className="card text-center">
+                    <h1 data-testid="title">{this.props.slides[this.state.count].title}</h1>
+                    <p data-testid="text">{this.props.slides[this.state.count].text}</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Slides;
